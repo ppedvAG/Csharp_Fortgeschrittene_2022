cd17b2db410ede9120cb3fc8b8ac626665dd945b
André <AndreR@ppedv.de>
2022-10-04 11:56:17 +0200

    Hallo Yield

---
+++ HalloYield/HalloYield/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+foreach (var item in GetStrings())
+{
+    Console.WriteLine(item);
+}
+
+
+IEnumerable<string> GetStrings()
+{
+    yield return "jkhwebf";
+    yield return "jkhwebf";
+    yield return "jkhwebf";
+    yield return "jkhwebf";
+}
+
+IEnumerable<string> GetStringsDoof()
+{
+    var result = new List<string>();
+
+    result.Add("jkhwebf");
+    result.Add("jkhwebf");
+    result.Add("jkhwebf");
+    result.Add("jkhwebf");
+    return result;
+}
