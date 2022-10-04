9ff91efc41a2165a020ac5ad723328f9d96f53da
André <AndreR@ppedv.de>
2022-10-04 15:17:50 +0200

    BooksClient

---
+++ BooksManager/BooksManager/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Windows;
+
+namespace BooksManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Such(object sender, RoutedEventArgs e)
+        {
+            var url = $"https://www.googleapis.com/books/v1/volumes?q={suchTb.Text}";
+
+            var http = new HttpClient();
+            var json = await http.GetStringAsync(url);
+
+            BooksResult result = JsonSerializer.Deserialize<BooksResult>(json);
+
+            myGrid.ItemsSource = result.items.Select(x=>x.volumeInfo);
+        }
+    }
+}
