4138fe4d12fc8e97b2cf42531910b5de40d0b37e
André <AndreR@ppedv.de>
2022-10-05 14:53:59 +0200

    Async await mit WPF

--- HalloAsync/HalloAsync/MainWindow.xaml.cs
+++ HalloAsync/HalloAsync/MainWindow.xaml.cs
@@ -1,19 +1,9 @@
 ﻿using Microsoft.Data.SqlClient;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace HalloAsync
 {
@@ -32,8 +22,22 @@ namespace HalloAsync
             for (int i = 0; i < 100; i++)
             {
                 pb1.Value = i;
-                Thread.Sleep(1000);
+
             }
+        }
+
+        private async void StartAsyncAwait(object sender, RoutedEventArgs e)
+        {
+            ((Button)sender).IsEnabled = false;
+
+            for (int i = 0; i < 100; i++)
+            {
+                pb1.Value = i;
+                await Task.Delay(100);
+            }
+
+            ((Button)sender).IsEnabled = true;
+
         }
 
         private void StartTask(object sender, RoutedEventArgs e)
@@ -53,6 +57,8 @@ namespace HalloAsync
 
         private void StartTaskmitTS(object sender, RoutedEventArgs e)
         {
+            cts = new CancellationTokenSource();
+
             var ts = TaskScheduler.FromCurrentSynchronizationContext();
             ((Button)sender).IsEnabled = false;
 
@@ -60,9 +66,14 @@ namespace HalloAsync
             {
                 for (int i = 0; i < 100; i++)
                 {
+                    if (cts.Token.IsCancellationRequested)
+                        break;
+                    //alternative
+                    //cts.Token.ThrowIfCancellationRequested();
+
                     Task.Factory.StartNew(() =>
                         pb1.Value = i,
-                        CancellationToken.None,
+                        cts.Token,
                         TaskCreationOptions.None,
                         ts);
                     Thread.Sleep(100);
@@ -73,9 +84,16 @@ namespace HalloAsync
                                  TaskContinuationOptions.None,
                                  ts);
         }
+        CancellationTokenSource cts;
+
+        private void Abort(object sender, RoutedEventArgs e)
+        {
+            cts?.Cancel(); 
+        }
 
         private void LoadFromDB(object sender, RoutedEventArgs e)
         {
+            cts = new CancellationTokenSource();
             pb1.IsIndeterminate = true;
             ((Button)sender).IsEnabled = false;
             var ts = TaskScheduler.FromCurrentSynchronizationContext();
@@ -89,8 +107,9 @@ namespace HalloAsync
                 cmd.CommandText = "SELECT count(*) FROM sys.Databases; WAITFOR DELAY '0:0:04'";
                 var count = cmd.ExecuteScalar();
 
-                return count;
+                cts.Token.ThrowIfCancellationRequested();
 
+                return count;
             });
             t.ContinueWith(t => MessageBox.Show($"{t.Result} DBs wurden gefunden"), CancellationToken.None, TaskContinuationOptions.OnlyOnRanToCompletion, ts);
             t.ContinueWith(t =>
@@ -98,9 +117,62 @@ namespace HalloAsync
                 pb1.IsIndeterminate = false;
                 ((Button)sender).IsEnabled = true;
             }, ts);
-            t.ContinueWith(t => MessageBox.Show(this, $"Fehler {t.Exception.InnerException.Message}"),
-                                 CancellationToken.None, TaskContinuationOptions.OnlyOnFaulted, ts);
+            t.ContinueWith(t =>
+            {
+                if (t.Exception.InnerException is OperationCanceledException)
+                    MessageBox.Show(this, $"Der Vorgang wurde erfolgreich abgebrochen");
+                else
+                    MessageBox.Show(this, $"Fehler {t.Exception.InnerException.Message}");
 
+            }, CancellationToken.None, TaskContinuationOptions.OnlyOnFaulted, ts);
+        }
+
+        private async void LoadFromDBAsyncAwait(object sender, RoutedEventArgs e)
+        {
+            cts = new CancellationTokenSource();
+            pb1.IsIndeterminate = true;
+            ((Button)sender).IsEnabled = false;
+
+            try
+            {
+                var conString = "Server=(localdb)\\Mssqllocaldb;Database=master;Trusted_Connection=true;";
+                var con = new SqlConnection(conString);
+                await con.OpenAsync(cts.Token);
+                var cmd = con.CreateCommand();
+                cmd.CommandText = "SELECT count(*) FROM sys.Databases; WAITFOR DELAY '0:0:10'";
+                var count = await cmd.ExecuteScalarAsync(cts.Token);
+
+                MessageBox.Show($"{count} DBs wurden gefunden");
+            }
+            catch (OperationCanceledException)
+            {
+                MessageBox.Show($"Aborted");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Fehler {ex.Message}");
+            }
+
+            pb1.IsIndeterminate = false;
+            ((Button)sender).IsEnabled = true;
+        }
+
+        private async void StartAltUndLangsam(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show($"{AlteUndLangsam(3)}");
+            MessageBox.Show($"{await AlteUndLangsamAsync(3)}");
+        }
+
+        public Task<long> AlteUndLangsamAsync(int zahl)
+        {
+            return Task.Run(() => AlteUndLangsam(zahl));
+        }
+
+
+        public long AlteUndLangsam(int zahl)
+        {
+            Thread.Sleep(5000);
+            return 90847893459785 * zahl;
         }
     }
 }
