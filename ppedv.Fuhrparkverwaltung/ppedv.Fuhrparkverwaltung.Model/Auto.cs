e7cdab301d77da89d8baff72e74684d0d9c684e2
André <AndreR@ppedv.de>
2022-10-06 11:06:37 +0200

    Fuhrpark Model

---
+++ ppedv.Fuhrparkverwaltung/ppedv.Fuhrparkverwaltung.Model/Auto.cs
@@ -0,0 +1,13 @@
+﻿namespace ppedv.Fuhrparkverwaltung.Model
+{
+    public class Auto
+    {
+        public int Id { get; set; }
+        public string Hersteller { get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;
+        public string? Farbe { get; set; }
+        public int Leistung { get; set; }
+        public DateTime Baujahr { get; set; }
+        public Garage? Garage { get; set; }
+    }
+}
---
+++ ppedv.Fuhrparkverwaltung/ppedv.Fuhrparkverwaltung.Model/Garage.cs
@@ -0,0 +1,9 @@
+﻿namespace ppedv.Fuhrparkverwaltung.Model
+{
+    public class Garage
+    {
+        public int Id { get; set; }
+        public string Ort { get; set; } = string.Empty;
+        public ICollection<Auto> Autos { get; set; } = new HashSet<Auto>();
+    }
+}
