edc4ddab1766e43ec60e8037a367cdb6c89685b2
André <AndreR@ppedv.de>
2022-10-06 15:05:16 +0200

    Dependency Injection

--- ppedv.Fuhrparkverwaltung/ppedv.Fuhrparkverwaltung.UI.DevConsole/Program.cs
+++ ppedv.Fuhrparkverwaltung/ppedv.Fuhrparkverwaltung.UI.DevConsole/Program.cs
@@ -1,15 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using Castle.DynamicProxy.Contributors;
+using Autofac;
+using ppedv.Fuhrparkverwaltung.Data.EfCore;
 using ppedv.Fuhrparkverwaltung.Logic.FuhrparkService;
 using ppedv.Fuhrparkverwaltung.Model;
 using ppedv.Fuhrparkverwaltung.Model.Contracts;
+using System.Reflection;
 
 Console.WriteLine("Hello, World!");
 
 var conString = "Server=(localdb)\\mssqllocaldb;Database=Fuhrpark_dev;Trusted_Connection=true";
-IRepository repo = new ppedv.Fuhrparkverwaltung.Data.EfCore.EfRepository(conString);
-FuhrparkManager fpm = new FuhrparkManager(repo);
 
+//DI per Refernce
+//IRepository repo = new ppedv.Fuhrparkverwaltung.Data.EfCore.EfRepository(conString);
+
+//DI per Reflection
+//var filePath = @"C:\Users\Fred\source\repos\ppedvAG\Csharp_Fortgeschrittene_2022\ppedv.Fuhrparkverwaltung\ppedv.Fuhrparkverwaltung.Data.EfCore\bin\Debug\net6.0\ppedv.Fuhrparkverwaltung.Data.EfCore.dll";
+//var ass = Assembly.LoadFrom(filePath);
+//Type typeMitIRepo = ass.GetTypes().FirstOrDefault(x => x.GetInterfaces().Contains(typeof(IRepository)));
+//var repo = (IRepository)Activator.CreateInstance(typeMitIRepo,conString);
+
+//DI per AutoFac
+var builder = new ContainerBuilder();
+//builder.RegisterType<EfRepository>().AsImplementedInterfaces();
+builder.Register(x => new EfRepository(conString)).As<IRepository>();
+var container = builder.Build();
+
+
+FuhrparkManager fpm = new FuhrparkManager(container.Resolve<IRepository>());
+var repo = container.Resolve<IRepository>();
 foreach (var auto in repo.GetAll<Auto>())
 {
     Console.WriteLine($"{auto.Hersteller} {auto.Model} {auto.Farbe} {auto.Leistung} {auto.Baujahr:d} [{auto.Garage?.Ort}]");
